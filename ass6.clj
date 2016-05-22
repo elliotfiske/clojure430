a27412c69af677f527e28c5ca99efbb5d88f45e1
elliotfiske <elliotfiske@gmail.com>
2016-05-22 16:30:34 -0700

    bout to pull fam

--- ass6.clj
+++ ass6.clj
@@ -1,8 +1,14 @@
 
 (defrecord NumC [n])
 (defrecord IdC [x])
+(defrecord IfC [condition then else])
+(defrecord BoolC [b])
 
 (defrecord NumV [n])
+(defrecord BoolV [b])
+
+(declare interp-fn)
+(declare interp)
 
 (def top-env {:+ :+Prim
               :- :-Prim
@@ -25,18 +31,40 @@
   {:+Prim my+
    :-Prim my-})
 
+
 ; Interp takes in an ExprC and evaluates it.
-; (: interp (ExprC -> Value))
+; (: interp (ExprC map -> Value))
 (def interp (fn [a env]
+   ; NumC
    (if (instance? NumC a)
       (NumV. (:n a))
-      (if (instance? IdC a)
 
+      ; IdC
+      (if (instance? IdC a)
         (if (contains? env (:x a))
           (get env (:x a))
           ((print "DFLY: No variable called ") (print (:x a))))
-        (print "Unimplemented :D")
+
+        ; IfC
+        (if (instance? IfC a)
+          ((def condition (interp (:condition a) env))
+          (if (instance? BoolV condition)
+            (if (:b condition))
+            (print "DFLY: An if statement conditioned
+                   evaluated to something other than true or false: ")
+            (print (interp (:condition a) env))))
+
+                ; LamC and AppC
+                (interp-fn a env)
+          )
+
    ))))
+
+
+; Helper function that interps LamCs and AppCs
+; (: interp-fn (ExprC map -> Value))
+(def interp-fn )
+
 
 (println (interp (NumC. 3) top-env)) ; (NumV 3)
 (println (interp (IdC. :+) top-env)) ; :+Prim
