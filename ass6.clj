601574e0bee0d0765f354b7bb38a6fe449bb6607
elliotfiske <elliotfiske@gmail.com>
2016-05-22 16:42:31 -0700

    Broken if

--- ass6.clj
+++ ass6.clj
@@ -35,36 +35,46 @@
 ; Interp takes in an ExprC and evaluates it.
 ; (: interp (ExprC map -> Value))
 (def interp (fn [a env]
-   ; NumC
-   (if (instance? NumC a)
-      (NumV. (:n a))
+   ; BoolC
+   (if (instance? BoolC a)
+     ;(BoolV. (:b a))
+     (BoolV. true)
 
-      ; IdC
-      (if (instance? IdC a)
-        (if (contains? env (:x a))
-          (get env (:x a))
-          ((print "DFLY: No variable called ") (print (:x a))))
+     ; NumC
+     (if (instance? NumC a)
+        (NumV. (:n a))
 
-        ; IfC
-        (if (instance? IfC a)
-          ((def condition (interp (:condition a) env))
-          (if (instance? BoolV condition)
-            (if (:b condition))
-            (print "DFLY: An if statement conditioned
-                   evaluated to something other than true or false: ")
-            (print (interp (:condition a) env))))
+        ; IdC
+        (if (instance? IdC a)
+          (if (contains? env (:x a))
+            (get env (:x a))
+            ((print "DFLY: No variable called ") (print (:x a))))
 
-                ; LamC and AppC
-                (interp-fn a env)
-          )
+          ; IfC
+          (if (instance? IfC a)
+            (((def condition (interp (:condition a) env)))
+             (print condition)
+            (if (instance? BoolV condition)
+              (if (:b condition)
+                   (interp (:then a) env)
+                   (interp (:else a) env))
+              ((print "DFLY: An if statement condition
+                     evaluated to something other than true or false: ")
+              (print condition))))
 
-   ))))
+              ; LamC and AppC
+              (interp-fn a env)
+            )
+
+   )))))
 
 
 ; Helper function that interps LamCs and AppCs
 ; (: interp-fn (ExprC map -> Value))
-(def interp-fn )
+; (def interp-fn )
 
 
 (println (interp (NumC. 3) top-env)) ; (NumV 3)
 (println (interp (IdC. :+) top-env)) ; :+Prim
+(println (interp (BoolC. true) top-env)) ; (BoolV true)
+(println (interp (IfC. (BoolC. false) (NumC. 4) (NumC. 5)) top-env)) ; (NumV 5)
